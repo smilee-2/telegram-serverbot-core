f06f869fa61b2c039499816d19c7546fb6c99315
DeniO <kozlovsky.den@mail.ru>
2025-01-05 22:09:45 +0800

    Minor fixes

--- modules/gpt.py
+++ modules/gpt.py
@@ -8,13 +8,13 @@ from queue import Queue
 q = Queue()
 
 # Отправка запроса в gpt. Если нейросеть не ответит, спросят следующую по списку
-def request(msg) -> str:
+def request(msg: str) -> str:
     q.put(msg)
     g4f.debug.logging = True
     g4f.debug.version_check = False
 
     client = Client(
-        provider=RetryProvider([Blackbox, DDG, GizAI, Mhystical, DarkAI], shuffle=False)
+        provider=RetryProvider([DDG, Blackbox, GizAI, Mhystical, DarkAI], shuffle=False)
     )
 
     response = client.chat.completions.create(
